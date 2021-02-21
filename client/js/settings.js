35b21fe304b027ac651f9f85c034bda87d102ee9
Maks Sliuzar <maks.slyzar@gmail.com>
2021-02-22 01:13:56 +0200

    add/done background

--- client/js/settings.js
+++ client/js/settings.js
@@ -1,6 +1,8 @@
+const PRODUCTION = false;
+
 window.onresize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    //canvas.width = window.innerWidth;
+    //canvas.height = window.innerHeight;
 }
 
 $(function(){
@@ -8,6 +10,8 @@ $(function(){
     //$("#chat").load("../components/chat.html"); 
     $("#playersList").load("../components/playersList.html"); 
     $("#connectForm").load("../components/connectForm.html");
+    $("#background").load("../components/background.html");
     $("#chat").load("../components/chat.html");
+    //$("#game").load("../components/game.html");
     //$("#chat").fadeOut();
 });
